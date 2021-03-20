6ebab6c6c5337e35f1b25230ec0f493f9fa3246f
S K B <47430686+shravan20@users.noreply.github.com>
2021-03-20 15:07:52 +0530

    Update init-fcm.js

--- src/init-fcm.js
+++ src/init-fcm.js
@@ -2,13 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/messaging";
 
 const initializedFirebaseApp = firebase.initializeApp({
-  messagingSenderId: "89467926110"
+  messagingSenderId: process.env.gcm_sender_id //"89467926110"
 });
 
 const messaging = initializedFirebaseApp.messaging();
 
 messaging.usePublicVapidKey(
-  "BOQEoxq2iHqG3sUa2p9KTFoPlzA_N9RNHOBDNvlBPRkY4FmOBsJUdD6Spu4oCAEC3aKtmaj1Jmjm8AzFWHqFFNI"
-);
+    process.env.server_key
+);//"BOQEoxq2iHqG3sUa2p9KTFoPlzA_N9RNHOBDNvlBPRkY4FmOBsJUdD6Spu4oCAEC3aKtmaj1Jmjm8AzFWHqFFNI"
+// Project Settings => Cloud Messaging => Web Push certificates
 
 export { messaging };
